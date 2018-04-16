151e0ec283a0d197021069cd0e864405a06b05ab
Gert Sønderby <gert.sonderby@orckestra.com>
2018-04-16 11:17:31 +0200

    Greatly improved typing of makeApiAction

--- src/actions/makeApiAction.js
+++ src/actions/makeApiAction.js
@@ -7,29 +7,34 @@ export const makeActionTypes = (name: string) => [
 	`${name}_FAILURE`,
 ];
 
-export const makeApiAction = (
+type Method = "GET" | "HEAD" | "POST" | "PUT" | "PATCH" | "DELETE" | "OPTIONS";
+type Options<S> = {
+	headers?: { [string]: string } | ((*) => { [string]: string }),
+	body?: Blob | ArrayBuffer | FormData | URLSearchParams | string,
+	options?: { [string]: string } | ((*) => { [string]: string }),
+	credentials?: "omit" | "same-origin" | "include",
+	bailout?: boolean | (S => boolean),
+};
+export type RSAAction<S> = {
+	[RSAA]: {
+		types: [string, string, string],
+		endpoint: string,
+		method: "GET" | "HEAD" | "POST" | "PUT" | "PATCH" | "DELETE" | "OPTIONS",
+	} & Options<S>,
+};
+
+export const makeApiAction = <S>(
 	name: string,
 	endpoint: string,
-	method: string = "GET",
-	configuration: {
-		headers?: { [string]: string },
-		body?: { [string]: any },
-		options?: { [string]: string | boolean },
-		credentials?: string,
-		bailout?: any => boolean,
-	} = {},
-) => {
-	const { headers, body, options, credentials, bailout } = configuration;
+	method: Method = "GET",
+	configuration: Options<S> = {},
+): RSAAction<S> => {
 	return {
 		[RSAA]: {
 			types: makeActionTypes(name),
 			endpoint,
 			method,
-			headers,
-			body,
-			options,
-			credentials,
-			bailout,
+			...configuration,
 		},
 	};
 };
