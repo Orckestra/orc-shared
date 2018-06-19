2504bb4ce1821a625a61576a0d4b4aab7f99fab1
Gert Sønderby <gert.sonderby@orckestra.com>
2018-06-19 11:32:54 +0200

    Added missing tests of view actions

---
+++ src/actions/view.test.js
@@ -0,0 +1,29 @@
+import { setValue, setStateField, VIEW_SET, VIEW_SET_FIELD } from "./view";
+
+describe("setValue", () => {
+	it("creates an action object", () =>
+		expect(
+			setValue,
+			"when called with",
+			["testfield", { thing: true }],
+			"to equal",
+			{
+				type: VIEW_SET,
+				payload: { name: "testfield", value: { thing: true } },
+			},
+		));
+});
+
+describe("setStateField", () => {
+	it("creates an action object", () =>
+		expect(
+			setStateField,
+			"when called with",
+			["testfield", "thing", false],
+			"to equal",
+			{
+				type: VIEW_SET_FIELD,
+				payload: { name: "testfield", field: "thing", value: false },
+			},
+		));
+});
