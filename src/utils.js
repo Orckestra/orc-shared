7c4464e3023639dca46d568e6bfe2c7f9ba4f4b9
Gert Sønderby <gert.sonderby@orckestra.com>
2018-11-22 11:17:50 +0100

    Add curry function to utils

--- src/utils.js
+++ src/utils.js
@@ -141,3 +141,9 @@ export const memoize = func => {
 		return cache.get(key);
 	};
 };
+
+/* Curries function calls, i.e. gathers up parameters, returning a
+function that will call the wrapped function with the curried parameters
+plus any further parameters */
+export const curry = (func, ...curried) => (...params) =>
+	func(...curried, ...params);
--- src/utils.test.js
+++ src/utils.test.js
@@ -14,6 +14,7 @@ import {
 	stripKey,
 	modulesToRoutes,
 	memoize,
+	curry,
 } from "utils";
 
 describe("safeGet", () => {
@@ -629,4 +630,85 @@ describe("memoize", () => {
 					{ args: [4] },
 				]),
 			));
+});
+
+describe("curry", () => {
+	let testFunc;
+	beforeEach(() => {
+		testFunc = (x, y) => `${x} and also ${y}`;
+	});
+
+	describe("0 < curried < arity", () => {
+		it("returns a function", () =>
+			expect(curry, "when called with", [testFunc, "foo"], "to be a function"));
+
+		it("calls wrapped function with curried parameters and call parameters", () =>
+			expect(
+				curry,
+				"when called with",
+				[testFunc, "foo"],
+				"when called with",
+				["bar"],
+				"to equal",
+				"foo and also bar",
+			));
+	});
+
+	describe("curried = 0", () => {
+		it("returns a function", () =>
+			expect(curry, "when called with", [testFunc], "to be a function"));
+
+		it("has no curried parameters", () =>
+			expect(
+				curry,
+				"when called with",
+				[testFunc],
+				"when called with",
+				["bar", "foo"],
+				"to equal",
+				"bar and also foo",
+			));
+	});
+
+	describe("curried = arity", () => {
+		it("returns a function", () =>
+			expect(
+				curry,
+				"when called with",
+				[testFunc, "foo", "bar"],
+				"to be a function",
+			));
+
+		it("calls wrapped function with curried parameters", () =>
+			expect(
+				curry,
+				"when called with",
+				[testFunc, "foo", "bar"],
+				"when called with",
+				["feep"],
+				"to equal",
+				"foo and also bar",
+			));
+	});
+
+	describe("curried > arity", () => {
+		it("returns a function", () =>
+			expect(
+				curry,
+				"when called with",
+				[testFunc, "feep", "meep", "foo"],
+				"to be a function",
+			));
+
+		it("calls function with curried parameters", () =>
+			expect(
+				curry,
+				"when called with",
+				[testFunc, "feep", "meep", "foo"],
+				"when called with",
+				["bar"],
+				"to equal",
+				"feep and also meep",
+			));
+	});
 });
