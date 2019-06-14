af6a4bb9c5abf291e5ce6b3a3ab540ee29cbb38d
Gert Sønderby <gert.sonderby@orckestra.com>
2019-06-14 13:50:58 +0200

    Text components will show a placeholder if missing values to show a translated message.

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,6 +1,37 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 import { FormattedMessage } from "react-intl";
 import withErrorBoundary from "../hocs/withErrorBoundary";
+
+export const messageContainsValues = message => {
+	const valRefs = message.defaultMessage.match(/(\{\w+\})/g);
+	if (!valRefs) return true;
+	const valNames = valRefs.map(ref => ref.replace(/[{}]/g, ""));
+	return (
+		!!message.values &&
+		valNames.every(
+			key => message.values[key] !== undefined && message.values[key] !== null,
+		)
+	);
+};
+
+const fadeCycle = keyframes`
+	0% {
+		opacity: 0.3;
+	}
+	100% {
+		opacity: 0.7;
+	}
+`;
+
+export const Placeholder = styled.span`
+	display: inline-block;
+	height: 1em;
+	width: 6em;
+	border-radius: 0.5em;
+	background-color: #999999;
+	animation: ${fadeCycle} 3s infinite alternate;
+`;
 
 const Text = ({ message, error }) => {
 	if (error || (!message && message !== "")) {
@@ -16,11 +47,15 @@ const Text = ({ message, error }) => {
 			</span>
 		);
 	}
-	return message.id ? (
-		<FormattedMessage {...message} />
-	) : (
-		<span>{message}</span>
-	);
+	if (message.id) {
+		if (messageContainsValues(message)) {
+			return <FormattedMessage {...message} />;
+		} else {
+			return <Placeholder />;
+		}
+	} else {
+		return <span>{message}</span>;
+	}
 };
 
 export default withErrorBoundary("Text")(Text);
--- src/components/Text.test.js
+++ src/components/Text.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
-import Text from "./Text";
+import Text, { messageContainsValues, Placeholder } from "./Text";
 
 describe("Text", () => {
 	it("renders a simple message", () =>
@@ -25,6 +25,37 @@ describe("Text", () => {
 			"renders elements",
 			"to render as",
 			<FormattedMessage id="test.msg" defaultMessage="Test message" />,
+		));
+
+	it("renders a translated message with values", () =>
+		expect(
+			<Text
+				message={{
+					id: "test.msg",
+					defaultMessage: "Test message {foo}",
+					values: { foo: 3 },
+				}}
+			/>,
+			"renders elements",
+			"to render as",
+			<FormattedMessage
+				id="test.msg"
+				defaultMessage="Test message {foo}"
+				values={{ foo: 3 }}
+			/>,
+		));
+
+	it("renders a translated message missing its values as a placeholder", () =>
+		expect(
+			<Text
+				message={{
+					id: "test.msg",
+					defaultMessage: "Test message {foo}",
+				}}
+			/>,
+			"renders elements",
+			"to render as",
+			<Placeholder />,
 		));
 
 	it("renders an error", () =>
@@ -57,5 +88,47 @@ describe("Text", () => {
 			>
 				Errored: No message provided
 			</span>,
+		));
+});
+
+describe("messageContainsValues", () => {
+	it("validates messages without needed values", () =>
+		expect(
+			messageContainsValues,
+			"called with",
+			[{ defaultMessage: "Test" }],
+			"to be true",
+		));
+
+	it("invalidates messages missing values", () =>
+		expect(
+			messageContainsValues,
+			"called with",
+			[{ defaultMessage: "Test {foo}" }],
+			"to be false",
+		));
+
+	it("invalidates messages missing some needed values", () =>
+		expect(
+			messageContainsValues,
+			"called with",
+			[{ defaultMessage: "Test {foo} {bar}", values: { foo: 1 } }],
+			"to be false",
+		));
+
+	it("invalidates messages with needed values set to null", () =>
+		expect(
+			messageContainsValues,
+			"called with",
+			[{ defaultMessage: "Test {foo} {bar}", values: { foo: 1, bar: null } }],
+			"to be false",
+		));
+
+	it("validates messages containing all needed values", () =>
+		expect(
+			messageContainsValues,
+			"called with",
+			[{ defaultMessage: "Test {foo} {bar}", values: { foo: 1, bar: "aaa" } }],
+			"to be true",
 		));
 });
