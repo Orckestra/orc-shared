3e7b13cf47ab5880f645da09d70906194fb6e069
Gert Sønderby <gert.sonderby@orckestra.com>
2019-06-27 10:25:22 +0200

    Much nicer loading placeholder

--- src/hocs/withAuthentication.js
+++ src/hocs/withAuthentication.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import { compose, branch, renderComponent, mapProps } from "recompose";
+import styled, { withTheme } from "styled-components";
+import { getThemeProp } from "../utils";
+import Placeholder from "../components/Placeholder";
 import { GET_AUTHENTICATION_PROFILE } from "../actions/authentication";
 
 const isAuthenticated = connect(state => ({
@@ -8,7 +11,23 @@ const isAuthenticated = connect(state => ({
 	authedUser: state.getIn(["authentication", "name"]),
 }));
 
-export const Loader = () => <div>Loading</div>; // Needs to be nicer
+const LoaderWrapper = styled.div`
+	display: flex;
+	width: 100%;
+	height: 100%;
+	flex-direction: column;
+	justify-content: center;
+`;
+
+export const Loader = withTheme(props => (
+	<LoaderWrapper>
+		<Placeholder
+			icon={getThemeProp(["icons", "loading"], "loading")(props)}
+			animate
+		/>
+	</LoaderWrapper>
+));
+
 export const Error = () => <h1>Not Logged In</h1>; // Should probably redirect to log in
 
 const withAuthentication = compose(
