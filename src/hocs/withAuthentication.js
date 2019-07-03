313e9e91e7fc77935a00cc9de73e0849ea37258b
Gert Sønderby <gert.sonderby@orckestra.com>
2019-07-03 14:39:10 +0200

    Improved error display at first load

--- src/hocs/withAuthentication.js
+++ src/hocs/withAuthentication.js
@@ -2,33 +2,53 @@ import React from "react";
 import { connect } from "react-redux";
 import { compose, branch, renderComponent, mapProps } from "recompose";
 import styled, { withTheme } from "styled-components";
-import { getThemeProp } from "../utils";
+import { getThemeProp, safeGet, unwrapImmutable } from "../utils";
 import Placeholder from "../components/Placeholder";
 import { GET_AUTHENTICATION_PROFILE } from "../actions/authentication";
+import { ERROR, LOGOUT } from "../reducers/request";
 
 const isAuthenticated = connect(state => ({
 	loading: state.getIn(["requests", GET_AUTHENTICATION_PROFILE]),
 	authedUser: state.getIn(["authentication", "name"]),
+	requestError: unwrapImmutable(state.getIn(["requests", ERROR])),
+	needLogin: state.getIn(["requests", LOGOUT]),
 }));
 
-const LoaderWrapper = styled.div`
+export const Wrapper = styled.div`
 	display: flex;
-	width: 100%;
+	width: 60%;
 	height: 100%;
+	padding: 0 20vw;
 	flex-direction: column;
 	justify-content: center;
 `;
 
 export const Loader = withTheme(props => (
-	<LoaderWrapper>
+	<Wrapper>
 		<Placeholder
 			icon={getThemeProp(["icons", "loading"], "loading")(props)}
 			animate
 		/>
-	</LoaderWrapper>
+	</Wrapper>
 ));
 
-export const Error = () => <h1>Not Logged In</h1>; // Should probably redirect to log in
+export const Error = ({ requestError, needLogin }) => {
+	if (needLogin) {
+		return (
+			<Wrapper>
+				<h1>Not logged in</h1>
+			</Wrapper>
+		);
+	}
+	return (
+		<Wrapper>
+			<h1>
+				{safeGet(requestError, "payload", "message") || "An error occurred"}
+			</h1>
+			Last failing action: <pre>{JSON.stringify(requestError, null, 2)}</pre>
+		</Wrapper>
+	);
+};
 
 const withAuthentication = compose(
 	isAuthenticated,
--- src/hocs/withAuthentication.test.js
+++ src/hocs/withAuthentication.test.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Immutable from "immutable";
 import { Provider } from "react-redux";
-import withAuthentication, { Loader, Error } from "./withAuthentication";
+import withAuthentication, {
+	Loader,
+	Wrapper,
+	Error,
+} from "./withAuthentication";
+import { ERROR, LOGOUT } from "../reducers/request";
 import { GET_AUTHENTICATION_PROFILE } from "../actions/authentication";
 
 const TestComp = () => <div />;
@@ -56,8 +61,10 @@ describe("withAuthentication", () => {
 			);
 	});
 
-	it("shows a load indicator component if authentication is ongoing", () => {
-		state = state.deleteIn(["authentication", "name"]);
+	it("shows an error screen if not logged in", () => {
+		state = state
+			.deleteIn(["authentication", "name"])
+			.setIn(["requests", LOGOUT], true);
 		return expect(withAuthentication, "called with", [TestComp])
 			.then(EnhComp =>
 				expect(
@@ -70,7 +77,87 @@ describe("withAuthentication", () => {
 			.then(render =>
 				expect(render, "not to contain", <TestComp />)
 					.and("not to contain", <Loader />)
-					.and("to contain", <Error />),
+					.and(
+						"to contain",
+						<Error>
+							<Wrapper>
+								<h1>Not logged in</h1>
+							</Wrapper>
+						</Error>,
+					),
+			);
+	});
+
+	it("shows an error screen if an error occured", () => {
+		state = state.deleteIn(["authentication", "name"]).setIn(
+			["requests", ERROR],
+			Immutable.fromJS({
+				type: "TEST_ACTION",
+				payload: { status: 404, message: "404 - NotFound" },
+			}),
+		);
+		return expect(withAuthentication, "called with", [TestComp])
+			.then(EnhComp =>
+				expect(
+					<Provider store={store}>
+						<EnhComp />
+					</Provider>,
+					"when deeply rendered",
+				),
+			)
+			.then(render =>
+				expect(render, "not to contain", <TestComp />)
+					.and("not to contain", <Loader />)
+					.and(
+						"to contain",
+						<Error>
+							<Wrapper>
+								<h1>404 - NotFound</h1>
+								Last failing action:{" "}
+								<pre>
+									{"{\n" +
+										'  "type": "TEST_ACTION",\n' +
+										'  "payload": {\n' +
+										'    "status": 404,\n' +
+										'    "message": "404 - NotFound"\n' +
+										"  }\n" +
+										"}"}
+								</pre>
+							</Wrapper>
+						</Error>,
+					),
+			);
+	});
+
+	it("shows an error screen if an error without payload occured", () => {
+		state = state.deleteIn(["authentication", "name"]).setIn(
+			["requests", ERROR],
+			Immutable.fromJS({
+				type: "TEST_ACTION",
+			}),
+		);
+		return expect(withAuthentication, "called with", [TestComp])
+			.then(EnhComp =>
+				expect(
+					<Provider store={store}>
+						<EnhComp />
+					</Provider>,
+					"when deeply rendered",
+				),
+			)
+			.then(render =>
+				expect(render, "not to contain", <TestComp />)
+					.and("not to contain", <Loader />)
+					.and(
+						"to contain",
+						<Error>
+							<Wrapper>
+								<h1>An error occurred</h1>
+								Last failing action:{" "}
+								<pre>{'{\n  "type": "TEST_ACTION"\n}'}</pre>
+							</Wrapper>
+						</Error>,
+					),
 			);
 	});
 });
