d2d79f7805bf8682fe0aad6356b086d5ba326b2b
Gert Sønderby <gert.sonderby@orckestra.com>
2019-12-09 10:50:20 +0100

    Test fix

--- src/components/Form/Inputs/Date.js
+++ src/components/Form/Inputs/Date.js
@@ -260,7 +260,7 @@ if (Intl.DateTimeFormat.prototype.formatToParts) {
 		})}
 	`;
 
-	let getDateUpdater = (update, part, value) => {
+	getDateUpdater = (update, part, value) => {
 		let prefix, suffix, partLength;
 		const match = value.match(/^(\d+)-(\d+)-(\d+)$/);
 		if (part === "year") {
