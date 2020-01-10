116c9e8ffb35cbe6f5bdf91cdeec1ca0dde6653a
Gert Sønderby <gert.sonderby@orckestra.com>
2020-01-10 12:22:35 +0100

    Created useToggle hook, equivalent to withToggle HOC

---
+++ docs/hooks.md
@@ -0,0 +1,7 @@
+# Hooks
+
+For documentation on how to use hooks, please see [the React documentation](https://reactjs.org/docs/hooks-intro.html).
+
+## `useToggle(init)`
+
+Returns `[flag, toggle, reset]` of a flag value with toggle and reset functions. The `init` parameter specifies the initial value of the flag.
---
+++ src/hooks/useToggle.test.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { mount, simulate } from "react-dom-testing";
+import useToggle from "./useToggle";
+
+const TestComp = ({ id, init }) => {
+	const [flag, toggle, reset] = useToggle(init);
+	return (
+		<div id={id} onClick={toggle} onKeyDown={reset}>
+			{flag ? 1 : 0}
+		</div>
+	);
+};
+
+describe("useToggle", () => {
+	it("provides a flag", () =>
+		expect(<TestComp />, "when mounted", "to satisfy", <div>0</div>));
+
+	it("can initialize the flag", () =>
+		expect(<TestComp init="1" />, "when mounted", "to satisfy", <div>1</div>));
+
+	it("provides a toggle function", () => {
+		const element = mount(<TestComp />);
+		expect(element, "to satisfy", <div>0</div>);
+		simulate(element, "click");
+		expect(element, "to satisfy", <div>1</div>);
+		simulate(element, "click");
+		expect(element, "to satisfy", <div>0</div>);
+	});
+
+	it("provides a reset function", () => {
+		const element = mount(<TestComp />);
+		expect(element, "to satisfy", <div>0</div>);
+		simulate(element, "click");
+		expect(element, "to satisfy", <div>1</div>);
+		simulate(element, "keyDown");
+		expect(element, "to satisfy", <div>0</div>);
+		simulate(element, "keyDown");
+		expect(element, "to satisfy", <div>0</div>);
+	});
+
+	it("keeps different elements separate", () => {
+		const element = mount(
+			<div>
+				<TestComp id="a" />
+				<TestComp id="b" />
+			</div>,
+		);
+		expect(
+			element,
+			"to satisfy",
+			<div>
+				<div id="a">0</div>
+				<div id="b">0</div>
+			</div>,
+		);
+		simulate(element, { type: "click", target: "#a" });
+		expect(
+			element,
+			"to satisfy",
+			<div>
+				<div id="a">1</div>
+				<div id="b">0</div>
+			</div>,
+		);
+		simulate(element, { type: "click", target: "#b" });
+		expect(
+			element,
+			"to satisfy",
+			<div>
+				<div id="a">1</div>
+				<div id="b">1</div>
+			</div>,
+		);
+		simulate(element, { type: "click", target: "#a" });
+		expect(
+			element,
+			"to satisfy",
+			<div>
+				<div id="a">0</div>
+				<div id="b">1</div>
+			</div>,
+		);
+	});
+});
