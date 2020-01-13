5af1651aaae4bfcc7b1b46537c2b0437ee58c20a
Gert Sønderby <gert.sonderby@orckestra.com>
2020-01-13 15:01:35 +0100

    Added useLoader hook

---
+++ src/hooks/useLoader.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+let hasWarned = false;
+const noCutout = () => {
+	if (!hasWarned) {
+		console.warn(
+			"useLoader hook called without cutout selector, loader will never be dispatched.",
+		);
+		hasWarned = true;
+	}
+	return true;
+};
+
+const useLoader = (loadActions, cutoutSelector = noCutout) => {
+	const dispatch = useDispatch();
+	const cutout = useSelector(cutoutSelector);
+	useEffect(() => {
+		const actions = Array.isArray(loadActions) ? loadActions : [loadActions];
+		if (!cutout) {
+			actions.forEach(action => dispatch(action));
+		}
+	}, [dispatch, loadActions, cutout]);
+};
+
+export default useLoader;
