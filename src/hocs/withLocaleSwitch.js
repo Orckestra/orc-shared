8f762920d257bc2ff6008c5b7aaa2897e83a6d55
Gert Sønderby <gert.sonderby@orckestra.com>
2020-02-17 14:55:13 +0100

    Switched deprecated HOCs to using hooks instead of routingConnector

--- src/hocs/withLocaleSwitch.js
+++ src/hocs/withLocaleSwitch.js
@@ -1,13 +1,14 @@
-import routingConnector from "./routingConnector";
+import { useDispatch } from "react-redux";
+import { withProps } from "recompose";
 import { changeLocale } from "../actions/locale";
 
 console.warn("withLocaleSwitch has been deprecated");
 
-const withLocaleSwitch = routingConnector(
-	() => ({}),
-	(dispatch, ownProps) => ({
-		onClick: () => dispatch(changeLocale(ownProps.locale)),
-	}),
-);
+const withLocaleSwitch = withProps(({ locale }) => {
+	const dispatch = useDispatch();
+	return {
+		onClick: () => dispatch(changeLocale(locale)),
+	};
+});
 
 export default withLocaleSwitch;
