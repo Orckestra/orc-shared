2e1c97fec6ce3e696313838c5a13c50ea506722f
Gert Sønderby <gert.sonderby@orckestra.com>
2020-02-17 15:47:37 +0100

    Added deprecation warnings to useLoader-replaced HOCs

--- src/hocs/withUpdateHandler.js
+++ src/hocs/withUpdateHandler.js
@@ -2,6 +2,11 @@ import { lifecycle } from "recompose";
 
 const withUpdateHandler = (handlerName, test = () => false) =>
 	lifecycle({
+		componentDidMount() {
+			console.warn(
+				"Higher order component withUpdateHandler has been deprecated in favor of React hook useLoader",
+			);
+		},
 		componentDidUpdate(prevProps, prevState) {
 			if (test({ ...prevProps, ...prevState }, this.props)) {
 				this.props[handlerName]();
