a93c9097efe220b19cd887e99a773d58c8175894
TONYFECTEAUOrc <tony.fecteau@orckestra.com>
2020-04-16 11:57:49 -0400

    Add a common column wrapper to be used with lists
    Story #36728

---
+++ src/components/ColumnWrapper.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+	flex: 0 1 100%;
+	display: flex;
+	flex-direction: column;
+	min-height: 0;
+	min-width: 0;
+`;
+
+export default Wrapper;
