56064cc401c82626db2b498adb146cd1c65ff651
Jocelyn Lefrancois <jocelyn.lefrancois@orckestra.com>
2020-04-29 17:14:53 -0400

    Clicking outside the preference panel should be disable when it is open.
    Story: #37085

--- src/components/AppFrame/Preferences.js
+++ src/components/AppFrame/Preferences.js
@@ -15,6 +15,7 @@ import { changeLocale } from "../../actions/locale";
 import { localizedAppOptionSelector } from "../../selectors/applications";
 import { currentLocaleOrDefault, cultureOptionList } from "../../selectors/locale";
 import { defaultAppId } from "../../selectors/settings";
+import withClickOutside from "../../hocs/withClickOutside";
 import { resetVersionInfo } from "../../actions/versionInfo";
 
 export const PREFS_NAME = "__prefsDialog";
@@ -27,9 +28,13 @@ export const PrefPanel = styled(Sidepanel)`
 	top: 40px;
 	box-shadow: -3px 2px 5px 0px rgba(0, 0, 0, 0.2);
 	z-index: 9998;
+`;
+
+export const Wrapper = withClickOutside(styled.div`
 	display: flex;
 	flex-direction: column;
-`;
+	height: 100%;
+`);
 
 export const Header = styled.div`
 	flex: 0 0 auto;
@@ -102,6 +107,11 @@ const usePreferenceSetup = () => {
 	};
 };
 
+export const clickOutsideHandler = e => {
+	e.preventDefault();
+	e.stopPropagation();
+};
+
 export const Preferences = ({ messages }) => {
 	const {
 		show,
@@ -114,38 +124,40 @@ export const Preferences = ({ messages }) => {
 	} = usePreferenceSetup();
 	return (
 		<PrefPanel in={show} width="380px" timeout={400}>
-			<Header>
-				<Text message={messages.preferences} />
-			</Header>
-			<PrefForm>
-				<FormContext.Provider value={{ values }}>
-					<FieldElements
-						fields={[
-							{
-								label: messages.language,
-								type: "Selector",
-								name: "language",
-								options: languageOptions,
-							},
-							{
-								label: messages.defaultApp,
-								type: "Selector",
-								name: "application",
-								options: applicationOptions,
-							},
-						]}
-						getUpdater={getUpdater}
-					/>
-				</FormContext.Provider>
-			</PrefForm>
-			<Footer>
-				<PrefButton id="cancelPrefs" onClick={clear}>
-					<Text message={messages.cancel} />
-				</PrefButton>
-				<PrefButton id="savePrefs" primary onClick={save}>
-					<Text message={messages.save} />
-				</PrefButton>
-			</Footer>
+			<Wrapper onClickOutside={clickOutsideHandler}>
+				<Header>
+					<Text message={messages.preferences} />
+				</Header>
+				<PrefForm>
+					<FormContext.Provider value={{ values }}>
+						<FieldElements
+							fields={[
+								{
+									label: messages.language,
+									type: "Selector",
+									name: "language",
+									options: languageOptions,
+								},
+								{
+									label: messages.defaultApp,
+									type: "Selector",
+									name: "application",
+									options: applicationOptions,
+								},
+							]}
+							getUpdater={getUpdater}
+						/>
+					</FormContext.Provider>
+				</PrefForm>
+				<Footer>
+					<PrefButton id="cancelPrefs" onClick={clear}>
+						<Text message={messages.cancel} />
+					</PrefButton>
+					<PrefButton id="savePrefs" primary onClick={save}>
+						<Text message={messages.save} />
+					</PrefButton>
+				</Footer>
+			</Wrapper>
 		</PrefPanel>
 	);
 };
