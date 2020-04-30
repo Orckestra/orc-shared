b4e5d498ed54dd047e034fafe30f70295761d71a
Gert Sønderby <gert.sonderby@orckestra.com>
2020-04-30 11:33:37 +0200

    Removed recompose from withInfiniteScroll HOC

--- src/hocs/withInfiniteScroll.js
+++ src/hocs/withInfiniteScroll.js
@@ -1,4 +1,4 @@
-import { withStateHandlers } from "recompose";
+import React, { useState, useCallback } from "react";
 import { debounce } from "../utils";
 
 export const getOnScroll = (
@@ -34,14 +34,39 @@ export const getOnScroll = (
 	};
 };
 
-export const withInfiniteScroll = withStateHandlers(
-	({ initialScrollTop = 0 }) => ({
-		scrollTop: initialScrollTop,
-		virtual: true,
-	}),
-	{
-		onScroll: getOnScroll,
-	},
-);
+const wrapGetOnScroll = (props, setScroll) => {
+	const onScroll = getOnScroll(null, props);
+	return event => {
+		const { scrollTop } = onScroll(event);
+		setScroll(scrollTop);
+	};
+};
+
+export const withInfiniteScroll = Comp => ({ initialScrollTop = 0, ...props }) => {
+	const [scrollTop, setScroll] = useState(initialScrollTop);
+	const {
+		onScroll = () => {},
+		scrollLoader,
+		loadTrigger = 200,
+		length = 0,
+		latestPage = 1,
+		pageLength = 20,
+	} = props;
+	const scrollHandler = useCallback(
+		wrapGetOnScroll(
+			{
+				onScroll,
+				scrollLoader,
+				loadTrigger,
+				length,
+				latestPage,
+				pageLength,
+			},
+			setScroll,
+		),
+		[setScroll, onScroll, scrollLoader, loadTrigger, length, latestPage, pageLength],
+	);
+	return <Comp {...props} onScroll={scrollHandler} scrollTop={scrollTop} virtual />;
+};
 
 export default withInfiniteScroll;
