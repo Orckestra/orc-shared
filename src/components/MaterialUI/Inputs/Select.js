395340fd17777db9f66e9c558620f803988636ef
Jocelyn Lefrancois <jocelyn.lefrancois@orckestra.com>
2020-06-04 14:13:16 -0400

    Added Select Wrapper component from Material-UI and localization helper functions
    Story: #36654

---
+++ src/components/MaterialUI/Inputs/Select.js
@@ -0,0 +1,89 @@
+import React from "react";
+import MenuItem from "@material-ui/core/MenuItem";
+import SelectMUI from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+	select: {
+		"& ul": {
+			//top: '20px',
+			minWidth: "175px",
+			maxHeight: "300px",
+		},
+		"& li": {
+			fontSize: 12,
+			"&:hover": {
+				backgroundColor: theme.palette.primary.main,
+			},
+		},
+	},
+	input: {
+		borderRadius: 4,
+		position: "relative",
+		backgroundColor: theme.palette.background.paper,
+		border: "1px solid #ced4da",
+		fontSize: 12,
+		minWidth: "150px",
+		maxWidth: "200px",
+		padding: "6px 6px 6px 6px",
+		transition: theme.transitions.create(["border-color", "box-shadow"]),
+		"&:focus": {
+			borderRadius: 4,
+			borderColor: "#80bdff",
+			boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+		},
+	},
+}));
+
+const MenuProps = {
+	getContentAnchorEl: null,
+	anchorOrigin: {
+		vertical: "bottom",
+		horizontal: "left",
+	},
+};
+
+const ChevronDown = props => (
+	<div
+		{...props}
+		style={{
+			fontSize: "32px",
+			fontFamily: "courier,monospace",
+			//fontWeight: 'bold',
+			color: "black",
+			height: "10px",
+			width: "20px",
+			marginRight: "5px",
+			padding: "12px 0 0",
+		}}
+	>
+		ˇ
+	</div>
+);
+
+const Select = ({ options, update, value }) => {
+	const classes = useStyles();
+
+	const handleChange = event => {
+		update(event.target.value);
+	};
+
+	return (
+		<SelectMUI
+			value={value}
+			onChange={handleChange}
+			disableUnderline={true}
+			IconComponent={ChevronDown}
+			data-test-id={"SelectMUI"}
+			MenuProps={{ classes: { paper: classes.select }, ...MenuProps }}
+		>
+			{options.map(option => (
+				<MenuItem key={option.value} value={option.value} data-test-id={option.value}>
+					{option.label}
+				</MenuItem>
+			))}
+		</SelectMUI>
+	);
+};
+
+export default Select;
---
+++ src/components/MaterialUI/Inputs/Select.test.js
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { mount } from "enzyme";
+import Select from "./Select";
+import SelectMUI from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import sinon from "sinon";
+import * as TestUtils from "react-dom/test-utils";
+
+describe("Select Component", () => {
+	let update, container;
+	beforeEach(() => {
+		container = document.createElement("div");
+		document.body.appendChild(container);
+		update = sinon.spy().named("update");
+	});
+	afterEach(() => {
+		document.body.removeChild(container);
+		container = null;
+	});
+
+	it("Renders Select component without errors", () => {
+		const options = [
+			{ value: "aValue", label: "aLabel" },
+			{ value: "anotherValue", label: "anotherLabel" },
+		];
+
+		const component = <Select options={options} update={update} value={"aValue"} />;
+
+		const mountedComponent = mount(component);
+		const expected = (
+			<SelectMUI>
+				<MenuItem>aLabel</MenuItem>
+				<MenuItem>anotherLabel</MenuItem>
+			</SelectMUI>
+		);
+
+		expect(mountedComponent.containsMatchingElement(expected), "to be truthy");
+	});
+
+	it("Select component hanldes change", () => {
+		const options = [
+			{ value: "aValue", label: "aLabel" },
+			{ value: "anotherValue", label: "anotherLabel" },
+		];
+
+		ReactDOM.render(
+			<Select options={options} update={update} value={"aValue"} />,
+			container,
+		);
+
+		const mousedownEvent = document.createEvent("MouseEvents");
+		mousedownEvent.initEvent("mousedown", true, false);
+
+		const clickEvent = document.createEvent("MouseEvents");
+		clickEvent.initEvent("click", true, false);
+
+		// Open the selector
+		const element = container.querySelector(".MuiSelect-root");
+		element.dispatchEvent(mousedownEvent);
+
+		const menuItem = document.querySelectorAll("li")[1] || null;
+		expect(menuItem, "not to be", null);
+
+		menuItem.dispatchEvent(clickEvent);
+		expect(update, "to have calls satisfying", [{ args: ["anotherValue"] }]);
+	});
+});
