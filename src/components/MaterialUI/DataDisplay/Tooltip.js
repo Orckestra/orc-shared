b1ef6e566a95c704852b713b660deab1fb404402
ruslan.avsenin <ruslan.avsenin@orckestra.com>
2020-06-12 00:23:43 +0300

    empty string was added to Tooltip value as default

--- src/components/MaterialUI/DataDisplay/Tooltip.js
+++ src/components/MaterialUI/DataDisplay/Tooltip.js
@@ -12,22 +12,15 @@ const useStyles = makeStyles(theme => ({
     whiteSpace: "nowrap",
     overflow: "hidden",
     textOverflow: "ellipsis",
-  },
-  tooltip: {
-    maxWidth: "100%",
-    "& p": {
-      fontSize: theme.typography.fontSize
-    }
   }
 }));
 
-const Tooltip = ({ value, maxWidth = 0 }) => {
+const Tooltip = ({ value = "", maxWidth = 0 }) => {
   const classes = useStyles({ maxWidth });
 
   const ref = useRef(null);
 
   const [isDisplayed, setIsDisplayed] = React.useState(false);
-
 
   useEffect(() => {
     setIsDisplayed(ref.current.clientWidth >= maxWidth);
@@ -38,7 +31,6 @@ const Tooltip = ({ value, maxWidth = 0 }) => {
       arrow
       title={value}
       disableHoverListener={!isDisplayed}
-      classes={{ tooltip: classes.tooltip }}
     >
       <div className={classNames(classes.noWrap, classes.container)} ref={ref}>
         {value}
