8d9dccfe4f6a11e78d13e50993c6a9ce22fc3b2f
Anastasiia Yenina <anastasiia.yenina@orckestra.com>
2020-06-25 13:00:45 +0300

    fixes for bug #38020 - Shipment metadata: the text is cut and see "..." event if the text is not truncated

--- src/components/MaterialUI/MultipleLinesText.js
+++ src/components/MaterialUI/MultipleLinesText.js
@@ -1,35 +1,25 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import classNames from "classnames";
-import { TextProps } from "./textProps";
-import lineClamp from "clamp-js";
+import TextClamp from "react-multi-clamp";
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+	clampedText: {
+		wordBreak: "break-word",
+	},
+}));
 
 const MultipleLinesText = ({ children, ...props }) => {
 	const classes = useStyles();
-	const textProps = props.textProps;
-	if (textProps != null && textProps instanceof TextProps === false) {
-		throw new TypeError("textProps property is not of type TextProps");
-	}
-	var customStyles = textProps?.get(TextProps.propNames.classes);
-
-	let elem = useRef(null);
-	useEffect(() => {
-		lineClamp(elem.current, { clamp: props.lineCount });
-	});
 
 	return (
-		<Typography
-			ref={elem}
-			classes={{
-				root: classNames(customStyles?.get(TextProps.ruleNames.root)),
-				body1: classNames(customStyles?.get(TextProps.ruleNames.body1)),
-			}}
+		<TextClamp
+			disableCssClamp={true}
+			clamp={props.lineCount}
+			className={classNames(classes.clampedText, props.className)}
 		>
 			{children}
-		</Typography>
+		</TextClamp>
 	);
 };
 
--- src/components/MaterialUI/MultipleLinesText.test.js
+++ src/components/MaterialUI/MultipleLinesText.test.js
@@ -1,29 +1,30 @@
 import React from "react";
 import { mount } from "enzyme";
 import MultipleLinesText from "./MultipleLinesText";
-import TextProps from "./textProps";
-import Typography from "@material-ui/core/Typography";
-import { ignoreConsoleError } from "./../../utils/testUtils";
+import TextClamp from "react-multi-clamp";
 
 describe("MultipleLinesText", () => {
 	const text =
 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas purus non augue tempor.";
 
 	it("Renders text", () => {
-		const component = <MultipleLinesText lineCount="2">{text}</MultipleLinesText>;
+		const lineCount = 2;
+		const component = <MultipleLinesText lineCount={lineCount}>{text}</MultipleLinesText>;
 
 		const mountedComponent = mount(component);
-		const expected = <Typography>{text}</Typography>;
+		const expected = (
+			<TextClamp disableCssClamp={true} clamp={lineCount}>
+				{text}
+			</TextClamp>
+		);
 
 		expect(mountedComponent.containsMatchingElement(expected), "to be truthy");
 	});
 
-	it("Add style to body1 ruleName", () => {
-		const multipleLinesTextProps = new TextProps();
+	it("Use style passed from component", () => {
 		const test = "testBody1";
-		multipleLinesTextProps.setStyle(TextProps.ruleNames.body1, test);
 		const component = (
-			<MultipleLinesText lineCount="2" textProps={multipleLinesTextProps}>
+			<MultipleLinesText lineCount={2} className={test}>
 				{text}
 			</MultipleLinesText>
 		);
@@ -31,29 +32,5 @@ describe("MultipleLinesText", () => {
 		const mountedComponent = mount(component);
 
 		expect(mountedComponent.exists(".testBody1"), "to be true");
-	});
-
-	it("Add style to root ruleName", () => {
-		const multipleLinesTextProps = new TextProps();
-		const test = "testRoot";
-		multipleLinesTextProps.setStyle(TextProps.ruleNames.body1, test);
-		const component = (
-			<MultipleLinesText lineCount="2" textProps={multipleLinesTextProps}>
-				{text}
-			</MultipleLinesText>
-		);
-
-		const mountedComponent = mount(component);
-
-		expect(mountedComponent.exists(".testRoot"), "to be true");
-	});
-
-	it("Fails if textProps has wrong type", () => {
-		ignoreConsoleError(() => {
-			const component = (
-				<MultipleLinesText textProps="Wrong Type">{text}</MultipleLinesText>
-			);
-			expect(() => mount(component), "to throw a", TypeError);
-		});
 	});
 });
