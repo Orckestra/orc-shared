0d0c8c76bdc90b3c14d0d9e670b8ce31bc46ddad
Anastasiia Yenina <anastasiia.yenina@orckestra.com>
2020-06-25 14:55:06 +0300

    removed unused hooks

--- src/components/MaterialUI/MultipleLinesText.js
+++ src/components/MaterialUI/MultipleLinesText.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import classNames from "classnames";
 import { TextProps } from "./textProps";
