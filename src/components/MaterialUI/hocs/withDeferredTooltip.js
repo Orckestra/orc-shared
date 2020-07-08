0bd6764a4b6b2bf0fb1e82096d6693cfe507ddc4
Crispried <ruslan.avsenin@gmail.com>
2020-07-08 12:39:52 +0300

    Features/37071 show tooltips for multiple lines tetx (#71)
    * 1. new property was added to deferredtooltip 2. multiplelinestext now is tooltipped
    
    * some tests were added
    
    * unnecessary property was removed from hoc in deferredtooltip
    
    Co-authored-by: ruslan.avsenin <ruslan.avsenin@orckestra.com>

--- src/components/MaterialUI/hocs/withDeferredTooltip.js
+++ src/components/MaterialUI/hocs/withDeferredTooltip.js
@@ -6,8 +6,7 @@ import { isString, isObject, isStringNullOrWhitespace, isReactComponent } from "
 // by itself then that property will be disappeared after using that HOC.
 // To fix it rename that property and pass titleValue just as a title for tooltip.
 
-const withDeferredTooltip = Comp => ({ titleValue, ...props }) => {
-
+const withDeferredTooltip = Comp => ({ titleValue, alwaysDisplay, ...props }) => {
   const [shouldBeTooltipped, setShouldBeTooltipped] = useState(false);
 
   const defaultComponent = (
@@ -18,7 +17,12 @@ const withDeferredTooltip = Comp => ({ titleValue, ...props }) => {
   );
 
   const makeComponentTooltipped = function (event) {
-    setShouldBeTooltipped(event.target.offsetWidth < event.target.scrollWidth);
+    if (alwaysDisplay) {
+      setShouldBeTooltipped(true);
+    }
+    else {
+      setShouldBeTooltipped(event.target.offsetWidth < event.target.scrollWidth);
+    }
   }
 
   if (titleValue == null) return <Comp {...props} />;
