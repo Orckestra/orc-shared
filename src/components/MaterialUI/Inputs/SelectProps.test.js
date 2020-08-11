ccbd91f5e2b1d21b0689c3acb2ed61820e214230
joclefrancois <34777072+joclefrancois@users.noreply.github.com>
2020-08-11 09:40:25 -0400

    Select drop down should be aligned on the left side (#88)
    Story: #39218
    
    Co-authored-by: Jocelyn Lefrancois <jocelyn.lefrancois@orckestra.com>

--- src/components/MaterialUI/Inputs/SelectProps.test.js
+++ src/components/MaterialUI/Inputs/SelectProps.test.js
@@ -2,13 +2,13 @@ import SelectProps from "./SelectProps";
 
 describe("Select Props", () => {
 	it("Contains necessary props keys", () => {
-		const propNames = ["update", "value", "sortType", "showAllValue", "showAllLabel"];
+		const propNames = ["update", "value", "sortType", "showAllValue", "showAllLabel", "positionOverride"];
 
 		expect(SelectProps.propNames, "to have keys", propNames);
 	});
 
 	it("Puts keys in component props map", () => {
-		const propNames = ["update", "value", "sortType", "showAllValue", "showAllLabel"];
+		const propNames = ["update", "value", "sortType", "showAllValue", "showAllLabel", "positionOverride"];
 
 		const selectProps = new SelectProps();
 
