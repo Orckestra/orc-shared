0bac5d4df74e9024e9f237cb3313c10f2002442c
Anastasiia Yenina <anastasiia.yenina@orckestra.com>
2020-09-03 18:30:38 +0300

    chandes for address and table helpers

--- src/utils/addressHelper.js
+++ src/utils/addressHelper.js
@@ -1,16 +1,5 @@
 export const buildInlineAddressFromObject = (address) => {
-  let inlineAddress = "";
-  const propsToUse = ["line1", "line2", "postalCode"];
-
-  for (let i = 0; i < propsToUse.length; i++) {
-    let tempProp = address[propsToUse[i]];
-    if (tempProp != null) {
-      inlineAddress += tempProp;
-      if (i < propsToUse.length - 1) {
-        inlineAddress += " ";
-      }
-    }
-  }
+  const inlineAddress = `${address.line1}${address.line2 ? ` ${address.line2}` : ""}, ${address.city}, ${address.regionCode}, ${address.postalCode}, ${address.country}`;
 
   return inlineAddress;
 }
--- src/utils/addressHelper.test.js
+++ src/utils/addressHelper.test.js
@@ -5,22 +5,28 @@ describe("buildInlineAddressFromObject", () => {
     const address = {
       line1: "Demer 26",
       line2: "Farmandstredet",
-      postalCode: "3110"
+      postalCode: "3110",
+      country: "Canada",
+      city: "Montreal",
+      regionCode: "CA-AB"
     }
 
     const inlineAddress = buildInlineAddressFromObject(address);
 
-    expect(inlineAddress, "to equal", "Demer 26 Farmandstredet 3110");
+    expect(inlineAddress, "to equal", "Demer 26 Farmandstredet, Montreal, CA-AB, 3110, Canada");
   });
 
   it("Retrieves correct address when  not all used props exists in passed object", () => {
     const address = {
       line1: "Demer 26",
-      postalCode: "3110"
+      postalCode: "3110",
+      country: "Canada",
+      city: "Montreal",
+      regionCode: "CA-AB"
     }
 
     const inlineAddress = buildInlineAddressFromObject(address);
 
-    expect(inlineAddress, "to equal", "Demer 26 3110");
+    expect(inlineAddress, "to equal", "Demer 26, Montreal, CA-AB, 3110, Canada");
   });
 });
