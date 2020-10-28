79fb42ba765b06025237e39d05b7a65b57179068
Alex <olek.honcharuk@orckestra.com>
2020-10-28 17:05:00 +0200

    SwitchProps tests

---
+++ src/components/MaterialUI/Inputs/SwitchProps.test.js
@@ -0,0 +1,42 @@
+import SwitchProps, { isSwitchProps } from "./SwitchProps";
+
+describe("Switch Props", () => {
+	it("Contains necessary props keys", () => {
+		const propNames = ["update", "value", "onCaption", "offCaption"];
+
+		expect(SwitchProps.propNames, "to have keys", propNames);
+	});
+
+	it("Puts keys in component props map", () => {
+		const propNames = ["update", "value", "onCaption", "offCaption"];
+
+		const switchProps = new SwitchProps();
+
+		const keys = Array.from(switchProps.componentProps.keys());
+
+		expect(keys, "to equal", propNames);
+	});
+});
+
+describe("isSwitchProps", () => {
+	it("Returns true if passed value is null", () => {
+		expect(isSwitchProps(null), "to be true");
+	});
+
+	it("Returns false if passed value is not object", () => {
+		expect(isSwitchProps("Not object"), "to be false");
+	});
+
+	it("Returns true if passed value type is SwitchProps", () => {
+		expect(isSwitchProps(new SwitchProps()), "to be true");
+	});
+
+	it("Returns true if passed value has property _isSwitchProps and it's true", () => {
+		expect(isSwitchProps({ _isSwitchProps: true }), "to be true");
+	});
+
+	it("Returns false if passed value has property _isSwitchProps and it's false or missing", () => {
+		expect(isSwitchProps({}), "to be false");
+		expect(isSwitchProps({ _isSwitchProps: false }), "to be false");
+	});
+});
