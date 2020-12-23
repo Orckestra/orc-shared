a9f0af18d4fa7c66d64d6093b06435394c884c05
Alex <olek.honcharuk@orckestra.com>
2020-12-23 12:47:22 +0200

    fix helper

--- src/utils/propertyHelper.js
+++ src/utils/propertyHelper.js
@@ -1,56 +1,31 @@
 const { Map } = require("immutable");
 
-const caseInsensitiveCompare = (prop, property) =>
-	prop.toLowerCase() === property.toLowerCase();
+const caseInsensitiveCompare = (prop, property) => prop.toLowerCase() === property.toLowerCase();
 
-const caseInsensitiveStartWithCompare = (prop, property) =>
-	prop.toLowerCase().startsWith(property.toLowerCase());
+const caseInsensitiveStartWithCompare = (prop, property) => prop.toLowerCase().startsWith(property.toLowerCase());
 
-export function getPropertyOrDefaultFromMap(
-	map,
-	property,
-	defaultValue,
-	ignoreCase,
-	startWith,
-) {
-	if (ignoreCase === false && startWith === false)
-		return map.get(property) || defaultValue;
+export function getPropertyOrDefaultFromMap(map, property, defaultValue, ignoreCase, startWith) {
+	if (ignoreCase === false && startWith === false) return map.get(property) || defaultValue;
 
-	const compareFunction = startWith
-		? caseInsensitiveStartWithCompare
-		: caseInsensitiveCompare;
+	const compareFunction = startWith ? caseInsensitiveStartWithCompare : caseInsensitiveCompare;
 
 	return map.find((value, key) => compareFunction(key, property)) || defaultValue;
 }
 
-export function getPropertyOrDefault(
-	obj,
-	property,
-	defaultValue,
-	ignoreCase = false,
-	startWith = false,
-) {
+export function getPropertyOrDefault(obj, property, defaultValue, ignoreCase = false, startWith = false) {
 	if (!obj) {
 		return defaultValue;
 	}
 
 	if (Map.isMap(obj)) {
-		return getPropertyOrDefaultFromMap(
-			obj,
-			property,
-			defaultValue,
-			ignoreCase,
-			startWith,
-		);
+		return getPropertyOrDefaultFromMap(obj, property, defaultValue, ignoreCase, startWith);
 	}
 
 	if (ignoreCase === false && startWith === false) {
 		return obj.hasOwnProperty(property) ? obj[property] : defaultValue;
 	}
 
-	const compareFunction = startWith
-		? caseInsensitiveStartWithCompare
-		: caseInsensitiveCompare;
+	const compareFunction = startWith ? caseInsensitiveStartWithCompare : caseInsensitiveCompare;
 
 	for (let prop in obj) {
 		if (obj.hasOwnProperty(prop) && compareFunction(prop, property)) {
@@ -66,13 +41,11 @@ export const isObjectContainsPropertyWithValue = (obj, propertyName, value) => {
 		return true;
 	}
 
-	let result = false;
-
 	for (let prop in obj) {
-		if (obj.hasOwnProperty(prop) && typeof obj[prop] === 'object') {
-			result = isObjectContainsPropertyWithValue(obj[prop], propertyName, value);
+		if (obj.hasOwnProperty(prop) && typeof obj[prop] === "object") {
+			if (isObjectContainsPropertyWithValue(obj[prop], propertyName, value)) return true;
 		}
 	}
 
-	return result;
+	return false;
 };
