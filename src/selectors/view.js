98fe1967b5cc59269da672daa1a1d5cd223e6372
Inna Boitsun <inna.boitsun@orckestra.com>
2020-12-24 20:28:30 +0200

    Modification indication for new tab

--- src/selectors/view.js
+++ src/selectors/view.js
@@ -1,92 +1,84 @@
 import { createSelector } from "reselect";
 import { selectCurrentModuleName } from "./navigation";
 import { isObjectContainsPropertyWithValue } from "./../utils/propertyHelper";
+import { tryGetNewEntityIdKey } from "./../utils/urlHelper";
 
 const modulesData = state => state.get("view");
 
 const editData = createSelector(modulesData, data => data.get("edit"));
 
-export const isEntityUnderEditing = (entityId) => createSelector(
-  editData,
-  selectCurrentModuleName,
-  (data, moduleName) => {
-    if (data != null) {
-      const dataJS = data.toJS();
-      const sections = dataJS[moduleName][entityId];
-      if (sections != null) {
-        return true;
-      }
-    }
-    return false;
-  },
-);
+export const isEntityUnderEditing = entityId =>
+	createSelector(editData, selectCurrentModuleName, (data, moduleName) => {
+		if (data != null) {
+			const dataJS = data.toJS();
+			const sections = dataJS[moduleName][entityId];
+			if (sections != null) {
+				return true;
+			}
+		}
+		return false;
+	});
 
-export const getModifiedSections = (entityId) => createSelector(
-  editData,
-  selectCurrentModuleName,
-  (data, moduleName) => {
-    return getModifiedSectionsFromModule(data, moduleName, entityId);
-  },
-);
+export const getModifiedSections = entityId =>
+	createSelector(editData, selectCurrentModuleName, (data, moduleName) => {
+		return getModifiedSectionsFromModule(data, moduleName, entityId);
+	});
 
 const getModifiedSectionsFromModule = (editData, moduleName, entityId) => {
-  const modifiedSections = [];
-  if (editData != null) {
-    const dataJS = editData.toJS();
-    const sections = dataJS[moduleName][entityId];
-    if (sections != null) {
-      const sectionsKeys = Object.keys(sections);
-      for (const sectionKey of sectionsKeys) {
-        const sectionModel = sections[sectionKey].model;
+	const modifiedSections = [];
+	if (editData != null) {
+		const dataJS = editData.toJS();
+		const sections = dataJS[moduleName][entityId];
+		if (sections != null) {
+			const sectionsKeys = Object.keys(sections);
+			for (const sectionKey of sectionsKeys) {
+				const sectionModel = sections[sectionKey].model;
 
-        if (sectionModel != null) {
-          const wasModified = isObjectContainsPropertyWithValue(sectionModel, "wasModified", true);
-          if (wasModified === true) {
-            modifiedSections.push(sectionKey);
-          }
-        }
-      }
-    }
-  }
-  return modifiedSections;
-}
+				if (sectionModel != null) {
+					const wasModified = isObjectContainsPropertyWithValue(sectionModel, "wasModified", true);
+					if (wasModified === true) {
+						modifiedSections.push(sectionKey);
+					}
+				}
+			}
+		}
+	}
+	return modifiedSections;
+};
 
-export const getModifiedTabs = (tabsParams) => createSelector(
-  editData,
-  selectCurrentModuleName,
-  (data, moduleName) => {
-    const modifiedTabs = [];
+export const getModifiedTabs = tabsParams =>
+	createSelector(editData, selectCurrentModuleName, (data, moduleName) => {
+		const modifiedTabs = [];
 
-    tabsParams.forEach(tabParams => {
-      for (let i = 0; i < tabParams.params.length; i++) {
-        const modifiedSections = getModifiedSectionsFromModule(data, moduleName, tabParams.params[i]);
-        if (modifiedSections.length > 0) {
-          modifiedTabs.push(tabParams.href);
-          break;
-        }
-      }
-    });
+		tabsParams.forEach(tabParams => {
+			let newEntityId = tryGetNewEntityIdKey(tabParams.href);
+			if (newEntityId) tabParams.params.push(newEntityId);
 
-    return modifiedTabs;
-  }
-)
+			for (let i = 0; i < tabParams.params.length; i++) {
+				const modifiedSections = getModifiedSectionsFromModule(data, moduleName, tabParams.params[i]);
+				if (modifiedSections.length > 0) {
+					modifiedTabs.push(tabParams.href);
+					break;
+				}
+			}
+		});
 
-export const getModifiedModels = (entityId) => createSelector(
-  editData,
-  selectCurrentModuleName,
-  (data, moduleName) => {
-    const models = {};
-    if (data != null) {
-      const dataJS = data.toJS();
-      const sections = dataJS[moduleName][entityId];
-      if (sections != null) {
-        const sectionsKeys = Object.keys(sections);
+		return modifiedTabs;
+	});
 
-        sectionsKeys.forEach(sectionKey => {
-          models[sectionKey] = sections[sectionKey].model;
-        });
-      }
-    }
-    return models;
-  }
-);
+export const getModifiedModels = entityId =>
+	createSelector(editData, selectCurrentModuleName, (data, moduleName) => {
+		const models = {};
+		if (data != null) {
+			const dataJS = data.toJS();
+			const sections = dataJS[moduleName][entityId];
+			if (sections != null) {
+				const sectionsKeys = Object.keys(sections);
+
+				sectionsKeys.forEach(sectionKey => {
+					models[sectionKey] = sections[sectionKey].model;
+				});
+			}
+		}
+		return models;
+	});
--- src/utils/urlHelper.js
+++ src/utils/urlHelper.js
@@ -1,9 +1,28 @@
 export const getValueFromUrlByKey = (url, path, key) => {
-  const valuesFromUrl = url.split('/');
+	const valuesFromUrl = url.split("/");
 
-  const keysFromPath = path.split('/');
+	const keysFromPath = path.split("/");
 
-  const keyIndex = keysFromPath.indexOf(key);
+	const keyIndex = keysFromPath.indexOf(key);
 
-  return valuesFromUrl[keyIndex];
-}
+	return valuesFromUrl[keyIndex];
+};
+
+export const NEW_ENTITY_URL_KEY = "new";
+/* expected url formats for new mode:
+	/new => new
+	/new/section => new
+	/new{number} => new{number}
+	/new{number}/section => new{number}	
+*/
+export const tryGetNewEntityIdKey = url => {
+	if (url.endsWith(`/${NEW_ENTITY_URL_KEY}`) || url.indexOf(`/${NEW_ENTITY_URL_KEY}/`) >= 0) return NEW_ENTITY_URL_KEY;
+
+	let match = url.match(`/(${NEW_ENTITY_URL_KEY}[0-9]+$)`);
+	if (match) return match[1];
+
+	match = url.match(`/(${NEW_ENTITY_URL_KEY}[0-9]+)(/+)`);
+	if (match) return match[1];
+
+	return undefined;
+};
