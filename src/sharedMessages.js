8c5eb0f67f4dbbc14766b8fb4f541068233e95df
Tony Fecteau <tfecteau@gmail.com>
2021-01-14 16:20:23 -0500

    Remove unneeded CR and fix test
    Story AB#42860

--- src/sharedMessages.js
+++ src/sharedMessages.js
@@ -83,7 +83,7 @@ const sharedMessages = defineMessages({
   },
   unsavedChanges: {
     id: "orc-shared.unsavedChanges",
-    defaultMessage: "This entity has unsaved changes. All changes will be lost.\r\n\r\nAre you sure you want to close it?"
+    defaultMessage: "This entity has unsaved changes. All changes will be lost.\n\nAre you sure you want to close it?"
   }
 });
 
