8ab76e6339c1f41a5b4492652330c07fa70c57ac
Tony Fecteau <tfecteau@gmail.com>
2021-01-14 15:23:10 -0500

    Correct formatting for files
    Story AB#42860

--- src/components/MaterialUI/DataDisplay/Modal.js
+++ src/components/MaterialUI/DataDisplay/Modal.js
@@ -1,76 +1,82 @@
 import React from "react";
-import ModalMui from "@material-ui/core/Modal";
+import ModalMui from '@material-ui/core/Modal';
 import { makeStyles } from "@material-ui/core/styles";
 import ModalProps, { isModalProps } from "./modalProps";
 
 const useStyles = makeStyles(theme => ({
-	modal: {
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	container: {
-		minWidth: theme.spacing(48),
-		borderRadius: "8px",
-		display: "flex",
-		flexDirection: "column",
-		backgroundColor: theme.palette.primary.contrastText,
-	},
-	title: {
-		height: theme.spacing(4),
-		backgroundColor: theme.palette.grey.lighter,
-		padding: `0 ${theme.spacing(2)}`,
-		borderRadius: `8px 8px 0 0`,
-		borderBottom: `1px solid ${theme.palette.primary.light}`,
-		display: "flex",
-		alignItems: "center",
-	},
-	message: {
-		minHeight: theme.spacing(10),
-		backgroundColor: theme.palette.primary.contrastText,
-		display: "flex",
-		alignItems: "center",
-		padding: `${theme.spacing(2)} ${theme.spacing(2)}`,
+  modal: {
+    display: "flex",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    minWidth: theme.spacing(48),
+    borderRadius: "8px",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: theme.palette.primary.contrastText,
+  },
+  title: {
+    height: theme.spacing(4),
+    backgroundColor: theme.palette.grey.lighter,
+    padding: `0 ${theme.spacing(2)}`,
+    borderRadius: `8px 8px 0 0`,
+    borderBottom: `1px solid ${theme.palette.primary.light}`,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  message: {
+    minHeight: theme.spacing(10),
+    backgroundColor: theme.palette.primary.contrastText,
+    display: "flex",
+    alignItems: "center",
 		whiteSpace: "pre",
-	},
-	actionPanel: {
-		padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
-		backgroundColor: theme.palette.grey.lighter,
-		borderRadius: `0 0 8px 8px`,
-		borderTop: `1px solid ${theme.palette.primary.light}`,
-	},
+    padding: `${theme.spacing(2)} ${theme.spacing(2)}`
+  },
+  actionPanel: {
+    padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+    backgroundColor: theme.palette.grey.lighter,
+    borderRadius: `0 0 8px 8px`,
+    borderTop: `1px solid ${theme.palette.primary.light}`
+  }
 }));
 
 const Modal = ({ message, modalProps }) => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	if (isModalProps(modalProps) === false) {
-		throw new TypeError("modalProps property is not of type ModalProps");
-	}
+  if (isModalProps(modalProps) === false) {
+    throw new TypeError("modalProps property is not of type ModalProps");
+  }
 
-	const open = modalProps?.get(ModalProps.propNames.open) ?? false;
-	const title = modalProps?.get(ModalProps.propNames.title);
-	const actionPanel = modalProps?.get(ModalProps.propNames.actionPanel);
-	const backdropClickCallback = modalProps?.get(ModalProps.propNames.backdropClickCallback);
+  const open = modalProps?.get(ModalProps.propNames.open) ?? false;
+  const title = modalProps?.get(ModalProps.propNames.title);
+  const actionPanel = modalProps?.get(ModalProps.propNames.actionPanel);
+  const backdropClickCallback = modalProps?.get(ModalProps.propNames.backdropClickCallback);
 
-	const model = (
-		<ModalMui
-			disablePortal
-			disableEnforceFocus
-			disableAutoFocus
-			open={open}
-			className={classes.modal}
-			onBackdropClick={backdropClickCallback != null ? () => backdropClickCallback() : null}
-		>
-			<div className={classes.container}>
-				<div className={classes.title}>{title}</div>
-				<div className={classes.message}>{message}</div>
-				<div className={classes.actionPanel}>{actionPanel}</div>
-			</div>
-		</ModalMui>
-	);
+  const model = (
+    <ModalMui
+      disablePortal
+      disableEnforceFocus
+      disableAutoFocus
+      open={open}
+      className={classes.modal}
+      onBackdropClick={backdropClickCallback != null ? () => backdropClickCallback() : null}
+    >
+      <div className={classes.container}>
+        <div className={classes.title}>
+          {title}
+        </div>
+        <div className={classes.message}>
+          {message}
+        </div>
+        <div className={classes.actionPanel}>
+          {actionPanel}
+        </div>
+      </div>
+    </ModalMui>
+  );
 
-	return model;
+  return model;
 };
 
 export default Modal;
--- src/sharedMessages.js
+++ src/sharedMessages.js
@@ -1,92 +1,90 @@
 import { defineMessages } from "react-intl";
 
 const sharedMessages = defineMessages({
-	scopeFilterPlaceholder: {
-		id: "orc-shared.scopeFilterPlaceholder",
-		defaultMessage: "Type a scope name",
-	},
-	signOut: {
-		id: "orc-shared.signOut",
-		defaultMessage: "Sign out",
-	},
-	preferences: {
-		id: "orc-shared.preferences",
-		defaultMessage: "Preferences",
-	},
-	about: {
-		id: "orc-shared.about",
-		defaultMessage: "About",
-	},
-	help: {
-		id: "orc-shared.help",
-		defaultMessage: "Help",
-	},
-	ccName: {
-		id: "orc-shared.ccName",
-		defaultMessage: "Orckestra Commerce Cloud",
-	},
-	ccVersion: {
-		id: "orc-shared.ccVersion",
-		defaultMessage: "Commerce Cloud {version}",
-	},
-	orcSharedVersion: {
-		id: "orc-shared.orcSharedVersion",
-		defaultMessage: "Orc-Shared Framework {version}",
-	},
-	orcScriptsVersion: {
-		id: "orc-shared.orcScriptsVersion",
-		defaultMessage: "Orc-Scripts Framework {version}",
-	},
-	orcSecretVersion: {
-		id: "orc-shared.orcSecretVersion",
-		defaultMessage: "Orc-Secret Framework {version}",
-	},
-	copyrightTermsNotice: {
-		id: "orc-shared.copyrightTermsNotice",
-		defaultMessage:
-			"This computer program is protected by copyright laws and international treaties. Unauthorized reproduction or redistribution of this program, or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law. Orckestra is a trademark of Orckestra Technologies Inc. All other trademarks are property of the respective owners.",
-	},
-	copyright: {
-		id: "orc-shared.copyright",
-		defaultMessage: "© 2021 Orckestra Technologies Inc.",
-	},
-	allRightsReserved: {
-		id: "orc-shared.allRightsReserved",
-		defaultMessage: "All rights reserved.",
-	},
-	defaultApp: {
-		id: "orc-shared.defaultApp",
-		defaultMessage: "Default application",
-	},
-	displayLanguage: {
-		id: "orc-shared.displayLanguage",
-		defaultMessage: "Display language",
-	},
-	save: {
-		id: "orc-shared.save",
-		defaultMessage: "Save",
-	},
-	ok: {
-		id: "orc-shared.ok",
-		defaultMessage: "Ok",
-	},
-	cancel: {
-		id: "orc-shared.cancel",
-		defaultMessage: "Cancel",
-	},
-	confirmation: {
-		id: "orc-shared.confirmation",
-		defaultMessage: "Confirmation",
-	},
-	notAvailable: {
-		id: "orc-shared.notAvailable",
-		defaultMessage: "N/A",
-	},
-	unsavedChanges: {
-		id: "orc-shared.unsavedChanges",
-		defaultMessage:
-			"This entity has unsaved changes. All changes will be lost.\r\n\r\nAre you sure you want to close it?",
-	},
+  scopeFilterPlaceholder: {
+    id: "orc-shared.scopeFilterPlaceholder",
+    defaultMessage: "Type a scope name"
+  },
+  signOut: {
+    id: "orc-shared.signOut",
+    defaultMessage: "Sign out"
+  },
+  preferences: {
+    id: "orc-shared.preferences",
+    defaultMessage: "Preferences"
+  },
+  about: {
+    id: "orc-shared.about",
+    defaultMessage: "About"
+  },
+  help: {
+    id: "orc-shared.help",
+    defaultMessage: "Help"
+  },
+  ccName: {
+    id: "orc-shared.ccName",
+    defaultMessage: "Orckestra Commerce Cloud"
+  },
+  ccVersion: {
+    id: "orc-shared.ccVersion",
+    defaultMessage: "Commerce Cloud {version}"
+  },
+  orcSharedVersion: {
+    id: "orc-shared.orcSharedVersion",
+    defaultMessage: "Orc-Shared Framework {version}"
+  },
+  orcScriptsVersion: {
+    id: "orc-shared.orcScriptsVersion",
+    defaultMessage: "Orc-Scripts Framework {version}"
+  },
+  orcSecretVersion: {
+    id: "orc-shared.orcSecretVersion",
+    defaultMessage: "Orc-Secret Framework {version}"
+  },
+  copyrightTermsNotice: {
+    id: "orc-shared.copyrightTermsNotice",
+    defaultMessage: "This computer program is protected by copyright laws and international treaties. Unauthorized reproduction or redistribution of this program, or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law. Orckestra is a trademark of Orckestra Technologies Inc. All other trademarks are property of the respective owners."
+  },
+  copyright: {
+    id: "orc-shared.copyright",
+    defaultMessage: "© 2021 Orckestra Technologies Inc."
+  },
+  allRightsReserved: {
+    id: "orc-shared.allRightsReserved",
+    defaultMessage: "All rights reserved."
+  },
+  defaultApp: {
+    id: "orc-shared.defaultApp",
+    defaultMessage: "Default application"
+  },
+  displayLanguage: {
+    id: "orc-shared.displayLanguage",
+    defaultMessage: "Display language"
+  },
+  save: {
+    id: "orc-shared.save",
+    defaultMessage: "Save"
+  },
+  ok: {
+    id: "orc-shared.ok",
+    defaultMessage: "Ok"
+  },
+  cancel: {
+    id: "orc-shared.cancel",
+    defaultMessage: "Cancel"
+  },
+  confirmation: {
+    id: "orc-shared.confirmation",
+    defaultMessage: "Confirmation"
+  },
+  notAvailable: {
+    id: "orc-shared.notAvailable",
+    defaultMessage: "N/A"
+  },
+  unsavedChanges: {
+    id: "orc-shared.unsavedChanges",
+    defaultMessage: "This entity has unsaved changes. All changes will be lost.\r\n\r\nAre you sure you want to close it?"
+  }
 });
 
 export default sharedMessages;
