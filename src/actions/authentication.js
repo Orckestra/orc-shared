785a49ee18a6608c05a4b27b138f45e0f84b1124
Alexandre Drouin <alexandre.drouin@orckestra.com>
2021-02-05 11:03:40 -0500

    Use generated API instead of buildUrl
     #43655

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -1,7 +1,7 @@
 import { makeActionTypes } from "./makeApiAction";
 import makeOrcApiAction from "./makeOrcApiAction";
-import { buildUrl } from "../utils/buildUrl";
 import { getBaseUrl } from "../buildStore";
+import { getUserPermissionsRequest, signOutRequest } from "./requestsApi";
 
 export const GET_AUTHENTICATION_PROFILE = "GET_AUTHENTICATION_PROFILE";
 
@@ -11,14 +11,13 @@ export const [
 	GET_AUTHENTICATION_PROFILE_FAILURE,
 ] = makeActionTypes(GET_AUTHENTICATION_PROFILE);
 
-export const getAuthProfile = () =>
-	makeOrcApiAction(GET_AUTHENTICATION_PROFILE, buildUrl(["authentication", "profile"]));
+export const getAuthProfile = () => makeOrcApiAction(GET_AUTHENTICATION_PROFILE, getUserPermissionsRequest.buildUrl());
 
 export const SIGN_OUT = "SIGN_OUT";
 
 export const [SIGN_OUT_REQUEST, SIGN_OUT_SUCCESS, SIGN_OUT_FAILURE] = makeActionTypes(SIGN_OUT);
 
 export const signOut = () =>
-	makeOrcApiAction(SIGN_OUT, buildUrl(["authentication", "signout"]), "POST", {
+	makeOrcApiAction(SIGN_OUT, signOutRequest.buildUrl(), signOutRequest.verb, {
 		body: { returnUrl: getBaseUrl() },
 	});
