29edfd52327b99c17d23ce9e8e900f706ffa40ad
Inna Boitsun <inna.boitsun@orckestra.com>
2021-03-29 12:48:11 +0300

    PR Comments

--- src/utils/setTranslationWithFallback.js
+++ src/utils/setTranslationWithFallback.js
@@ -1,13 +1,11 @@
 import { flatten } from "./flatten";
 
-export const setTranslationWithFallbackField = (locale, obj, fallbackField, ...field) => {
-	if (!obj || !obj.getIn(flatten([field]))) return obj;
-	const value = obj.getIn(flatten([field, locale])) || obj.get(fallbackField) || "";
-	return obj.setIn(flatten([field]), value);
-};
-
 export const setTranslationWithFallbackValue = (locale, obj, fallbackValue, ...field) => {
 	if (!obj || !obj.getIn(flatten([field]))) return obj;
 	const value = obj.getIn(flatten([field, locale])) || fallbackValue || "";
 	return obj.setIn(flatten([field]), value);
+};
+
+export const setTranslationWithFallbackField = (locale, obj, fallbackField, ...field) => {
+	return setTranslationWithFallbackValue(locale, obj, obj?.get(fallbackField), ...field);
 };
