78981fad6bd15b38ca7fba2520de06db9540f010
ruslan.avsenin <ruslan.avsenin@orckestra.com>
2021-04-05 14:43:21 +0300

    dynamic edit state was implemented

--- src/hooks/useEditState.js
+++ src/hooks/useEditState.js
@@ -5,6 +5,8 @@ import { useDispatchWithModulesData } from "./../hooks/useDispatchWithModulesDat
 import { setEditModelField, setEditModelFieldError } from "./../actions/view";
 import { validationRules } from "../utils/modelValidationHelper";
 import { useSelectorAndUnwrap } from "./useSelectorAndUnwrap";
+import { get } from "lodash";
+import Immutable from "immutable";
 
 // if you need to override default validation rules just pass new rules with default keys
 // as properties of extendedValidationRules
@@ -45,6 +47,74 @@ export const useEditState = (entityId, sectionName, extendedValidationRules = {}
 
 				if (isValid === false) {
 					dispatchWithModulesData(setEditModelFieldError, [keys, errorType, entityId, sectionName]);
+
+					hasAnyValidationErrors = true;
+					return;
+				}
+			});
+
+			return !hasAnyValidationErrors;
+		};
+
+		return {
+			state: editState,
+			update: updateEditState,
+			reset: resetEditState,
+			isValid: isEditStateValid,
+		};
+	};
+
+	return useFieldState;
+};
+
+// if you need to override default validation rules just pass new rules with default keys
+// as properties of extendedValidationRules
+export const useDynamicEditState = (entityId, sectionName, extendedValidationRules = {}) => {
+	const currentModuleName = useSelector(selectCurrentModuleName);
+	const dispatchWithModulesData = useDispatchWithModulesData();
+
+	const mergedValidationRules = { ...validationRules, ...extendedValidationRules };
+
+	const useFieldState = (keys, initialValue = "", saveInitialValueToEditState = false) => {
+		const stateValue = useSelectorAndUnwrap(state =>
+			state.getIn(["view", "edit", currentModuleName, entityId, sectionName, "model", ...keys]),
+		);
+
+		useEffect(() => {
+			if (saveInitialValueToEditState && stateValue == null) {
+				dispatchWithModulesData(setEditModelField, [keys, initialValue, initialValue, entityId, sectionName]);
+			}
+		}, [initialValue, keys, saveInitialValueToEditState, stateValue]);
+
+		const editState = (path = []) => {
+			const keyPath = path.join(".");
+
+			return get(stateValue?.value, keyPath) || { value: initialValue };
+		};
+
+		const updateEditState = (newValue, path = [], errorTypes = []) => {
+			const fullPath = path.length === 0 ? [...keys] : [...keys, "value", ...path];
+
+			dispatchWithModulesData(setEditModelField, [fullPath, newValue, initialValue, entityId, sectionName]);
+
+			isEditStateValid(newValue, path, errorTypes);
+		};
+
+		const resetEditState = (path = []) => {
+			const fullPath = path.length === 0 ? [...keys] : [...keys, "value", ...path];
+			dispatchWithModulesData(setEditModelField, [fullPath, initialValue, initialValue, entityId, sectionName]);
+		};
+
+		const isEditStateValid = (value, path = [], errorTypes = []) => {
+			const valueToValidate = value ?? editState(path).value;
+			let hasAnyValidationErrors = false;
+			const fullPath = path.length === 0 ? [...keys] : [...keys, "value", ...path];
+
+			errorTypes.forEach(errorType => {
+				const isValid = mergedValidationRules[errorType](valueToValidate);
+
+				if (isValid === false) {
+					dispatchWithModulesData(setEditModelFieldError, [fullPath, errorType, entityId, sectionName]);
 
 					hasAnyValidationErrors = true;
 					return;
--- src/utils/modelValidationHelper.js
+++ src/utils/modelValidationHelper.js
@@ -1,6 +1,6 @@
 import { isStringNullOrWhitespace } from "./propertyValidator";
 import { validationErrorTypes } from "../constants";
-import { isEmpty } from "lodash";
+import { isEmpty, get } from "lodash";
 
 export const validationRules = {
 	[validationErrorTypes.fieldIsRequired]: value => (isStringNullOrWhitespace(value) ? false : true),
@@ -12,7 +12,18 @@ export const showError = field => {
 	return field.error != null;
 };
 
-export const hasValidationErrors = editState => {
+// export const test1 = {
+// 	some: {
+// 		map: [
+// 			{
+// 				path: "a.b",
+// 				elementsPath: ["c", "d"],
+// 			},
+// 		],
+// 	},
+// };
+
+export const hasValidationErrors = (editState, validationMap) => {
 	if (isEmpty(editState)) return true;
 
 	const fields = Object.keys(editState);
@@ -25,6 +36,32 @@ export const hasValidationErrors = editState => {
 			hasAnyValidationErrors = true;
 		}
 	});
+
+	if (validationMap) {
+		const mapKeys = Object.keys(validationMap);
+		mapKeys.forEach(mapKey => {
+			validationMap[mapKey].map.forEach(node => {
+				const fullNodePath = mapKey.concat(".", node.path);
+
+				if (node.elements) {
+					node.elements.forEach(element => {
+						const fullElementPath = `${fullNodePath}.${element.path}`;
+						const errorTypes = [...validationMap[mapKey].errorTypes, ...node.errorTypes, ...element.errorTypes];
+						const isValid = editState[mapKey].isValid(null, fullElementPath, errorTypes);
+						if (isValid === false) {
+							hasAnyValidationErrors = true;
+						}
+					});
+				} else {
+					const errorTypes = [...validationMap[mapKey].errorTypes, ...node.errorTypes];
+					const isValid = editState[mapKey].isValid(null, fullNodePath, errorTypes);
+					if (isValid === false) {
+						hasAnyValidationErrors = true;
+					}
+				}
+			});
+		});
+	}
 
 	return hasAnyValidationErrors;
 };
