887bd8123a288f78d2366441f40738e6bc7aa573
Alexandre Drouin <alexandre.drouin@orckestra.com>
2021-04-13 11:34:46 -0400

    Add useCallback to useViewState
    This will allow the return values to be used with useEffect
    
     #41583

--- src/hooks/useViewState.js
+++ src/hooks/useViewState.js
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { unwrapImmutable } from "../utils";
 import { setValue, setStateField } from "../actions/view";
@@ -5,8 +6,11 @@ import { setValue, setStateField } from "../actions/view";
 const useViewState = name => {
 	const viewState = unwrapImmutable(useSelector(state => state.getIn(["view", name]))) || {};
 	const dispatch = useDispatch();
-	const updateViewState = (fieldName, value) => dispatch(setStateField(name, fieldName, value));
-	const resetViewState = defaultValue => dispatch(setValue(name, defaultValue || {}));
+	const updateViewState = useCallback((fieldName, value) => dispatch(setStateField(name, fieldName, value)), [
+		dispatch,
+		name,
+	]);
+	const resetViewState = useCallback(defaultValue => dispatch(setValue(name, defaultValue || {})), [dispatch, name]);
 	return [viewState, updateViewState, resetViewState];
 };
 
