e5ca2d05ec298d301539f18685932712e407479e
Jocelyn Lefrancois <jocelyn.lefrancois@orckestra.com>
2021-04-28 10:06:57 -0400

    Modifications after review
    Story: #43800

--- src/components/MaterialUI/DataDisplay/PredefinedElements/ActionModal.js
+++ src/components/MaterialUI/DataDisplay/PredefinedElements/ActionModal.js
@@ -3,17 +3,12 @@ import Modal from "./../Modal";
 import ModalProps from "./../modalProps";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import sharedMessages from "../../../../sharedMessages";
 import { FormattedMessage } from "react-intl";
 
 const useStyles = makeStyles(theme => ({
 	actionPanel: {
 		float: "right",
-	},
-	message: {
-		maxWidth: "480px",
-		wordWrap: "normal",
 	},
 }));
 
@@ -29,11 +24,6 @@ const ActionModal = ({
 	const modalProps = new ModalProps();
 
 	const titleComponent = title || <FormattedMessage {...sharedMessages.confirmation} />;
-	const messageComponent = (
-		<div className={classes.message}>
-			<Typography children={message} />
-		</div>
-	);
 
 	modalProps.set(ModalProps.propNames.title, titleComponent);
 	modalProps.set(ModalProps.propNames.open, open);
@@ -57,7 +47,7 @@ const ActionModal = ({
 
 	modalProps.set(ModalProps.propNames.actionPanel, actionPanel);
 
-	return <Modal message={messageComponent} modalProps={modalProps} />;
+	return <Modal message={message} modalProps={modalProps} />;
 };
 
 export default ActionModal;
