617df268405cfa9b7b95994d12a55ce8d382620c
Alexandre Drouin <alexandre.drouin@orckestra.com>
2021-04-29 14:38:54 -0400

    Allow parseGuid to parse 36 char guids

--- src/utils/parseHelper.test.js
+++ src/utils/parseHelper.test.js
@@ -7,8 +7,18 @@ import {
 } from "./parseHelper";
 
 describe("parseGuid", () => {
-	it("Retrieves parsed guid if passed string is correct guid", () => {
+	it("Retrieves parsed guid without hyphens if passed string is correct guid", () => {
 		const guid = "9d447e3eb6f14082b1847a1b1854e78f";
+
+		const parsedGuid = parseGuid(guid);
+
+		const expectedParsedGuid = "9d447e3e-b6f1-4082-b184-7a1b1854e78f";
+
+		expect(parsedGuid, "to be", expectedParsedGuid);
+	});
+
+	it("Retrieves parsed guid with hyphens if passed string is correct guid", () => {
+		const guid = "9d447e3e-b6f1-4082-b184-7a1b1854e78f";
 
 		const parsedGuid = parseGuid(guid);
 
@@ -19,6 +29,22 @@ describe("parseGuid", () => {
 
 	it("Retrieves empty string if passed string is not correct guid", () => {
 		const guid = "Not guid";
+
+		const parsedGuid = parseGuid(guid);
+
+		expect(parsedGuid, "to be", "");
+	});
+
+	it("Retrieves empty string if passed string is null", () => {
+		const guid = null;
+
+		const parsedGuid = parseGuid(guid);
+
+		expect(parsedGuid, "to be", "");
+	});
+
+	it("Retrieves empty string if passed string is undefined", () => {
+		const guid = undefined;
 
 		const parsedGuid = parseGuid(guid);
 
