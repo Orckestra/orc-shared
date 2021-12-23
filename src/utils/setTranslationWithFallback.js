0ce5d5f2499bfca8d5a9db27e46c75782e402b87
Inna Boitsun <inna.boitsun@orckestra.com>
2021-12-23 15:31:51 +0200

    AB#55942 = fix bug with displayName for Scope - use logic: 1) find by language-culture 2) find by language 3) use fallback

--- src/utils/setTranslationWithFallback.js
+++ src/utils/setTranslationWithFallback.js
@@ -2,7 +2,24 @@ import { flatten } from "./flatten";
 
 export const setTranslationWithFallbackValue = (locale, obj, fallbackValue, ...field) => {
 	if (!obj) return obj;
-	const value = obj.getIn(flatten([field, locale])) || fallbackValue || "";
+	let localeValue = obj.getIn(flatten([field, locale]));
+	if (!localeValue && locale.includes("-")) {
+		let fieldValue = obj.getIn(flatten([field]));
+		if (fieldValue) {
+			let fieldKeys = fieldValue.keys();
+			let language = locale.substring(0, locale.indexOf("-"));
+
+			for (let key of fieldKeys) {
+				if (key !== locale && key.startsWith(language)) {
+					localeValue = obj.getIn(flatten([field, key]));
+					if (localeValue) {
+						break;
+					}
+				}
+			}
+		}
+	}
+	const value = localeValue || fallbackValue || "";
 	return obj.setIn(flatten([field]), value);
 };
 
