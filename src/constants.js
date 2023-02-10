01a923767fc9d087f15b37dee9bd74ea707324ca
Tony Fecteau <tony.fecteau@orckestra.com>
2023-02-10 11:47:53 -0500

    Revert "Returns module updated (#483)"
    This reverts commit e052850cade556626bf77c39c6a314a2b00fc352.

--- src/constants.js
+++ src/constants.js
@@ -19,7 +19,7 @@ export const platformRoles = {
 export const roleGroups = {
 	Products: "Products",
 	Orders: "Orders",
-	Returns: "Returns",
+	OrderReturn: "OrderReturn",
 	Marketing: "Marketing",
 	Analytics: "Analytics",
 	Shopping: "Shopping",
