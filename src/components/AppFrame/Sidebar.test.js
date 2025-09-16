adec442202780301458a69ed961ca01f1e2da47e
Alexandre Drouin <alexandre.drouin@orckestra.com>
2025-09-16 12:49:36 -0400

    fix logo

--- src/components/AppFrame/Sidebar.test.js
+++ src/components/AppFrame/Sidebar.test.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import Immutable from "immutable";
-import { TestWrapper } from "../../utils/testUtils";
-import Sidebar, { EnhancedMenuItem, MenuToggle, Logo } from "./Sidebar";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import Sidebar, { Bar, EnhancedMenuItem, MenuToggle, Logo } from "./Sidebar";
 import MenuItem from "./MenuItem";
 
 describe("Sidebar", () => {
@@ -41,88 +43,106 @@ describe("Sidebar", () => {
 
 	it("renders a sidebar with app selector and page menu", () =>
 		expect(
-			<TestWrapper
-				provider={{ store }}
-				stylesProvider
-				muiThemeProvider
-				memoryRouter={{ initialEntries: ["/Global/second"] }}
-			>
-				<Sidebar modules={modules} />
-			</TestWrapper>,
+			<Provider store={store}>
+				<ThemeProvider theme={{}}>
+					<MemoryRouter initialEntries={["/Global/second"]}>
+						<Sidebar modules={modules} />
+					</MemoryRouter>
+				</ThemeProvider>
+			</Provider>,
 			"when mounted",
 			"to satisfy",
-			<TestWrapper
-				provider={{ store }}
-				stylesProvider
-				muiThemeProvider
-				memoryRouter={{ initialEntries: ["/Global/second"] }}
-			>
-				<div>
-					<MenuToggle />
-					<EnhancedMenuItem icon="cars" title="First page" id="first" label="First page" />
-					<EnhancedMenuItem icon="person" title="Second page" id="second" label="Second page" />
-					<Logo />
-				</div>
-			</TestWrapper>,
+			<Provider store={store}>
+				<ThemeProvider theme={{}}>
+					<MemoryRouter initialEntries={["/Global/second"]}>
+						<Bar>
+							<MenuToggle />
+							<EnhancedMenuItem icon="cars" title="First page" id="first" label="First page" />
+							<EnhancedMenuItem icon="person" title="Second page" id="second" label="Second page" />
+							<Logo />
+						</Bar>
+					</MemoryRouter>
+				</ThemeProvider>
+			</Provider>,
 		));
 
 	it("renders a sidebar showing alerts", () =>
 		expect(
-			<TestWrapper
-				provider={{ store }}
-				stylesProvider
-				muiThemeProvider
-				memoryRouter={{ initialEntries: ["/Global/second"] }}
-			>
-				<Sidebar modules={modules} activeModules={{ first: { type: "confirm" } }} />
-			</TestWrapper>,
+			<Provider store={store}>
+				<ThemeProvider theme={{}}>
+					<MemoryRouter initialEntries={["/Global/second"]}>
+						<Sidebar modules={modules} activeModules={{ first: { type: "confirm" } }} />
+					</MemoryRouter>
+				</ThemeProvider>
+			</Provider>,
 			"when mounted",
 			"to satisfy",
-			<TestWrapper
-				provider={{ store }}
-				stylesProvider
-				muiThemeProvider
-				memoryRouter={{ initialEntries: ["/Global/second"] }}
-			>
-				<div>
-					<MenuToggle />
-					<EnhancedMenuItem icon="cars" title="First page" id="first" label="First page" alert={{ type: "confirm" }} />
-					<EnhancedMenuItem icon="person" title="Second page" id="second" label="Second page" />
-					<Logo />
-				</div>
-			</TestWrapper>,
+			<Provider store={store}>
+				<ThemeProvider theme={{}}>
+					<MemoryRouter initialEntries={["/Global/second"]}>
+						<Bar>
+							<MenuToggle />
+							<EnhancedMenuItem
+								icon="cars"
+								title="First page"
+								id="first"
+								label="First page"
+								alert={{ type: "confirm" }}
+							/>
+							<EnhancedMenuItem icon="person" title="Second page" id="second" label="Second page" />
+							<Logo />
+						</Bar>
+					</MemoryRouter>
+				</ThemeProvider>
+			</Provider>,
 		));
 
 	it("renders an open sidebar", () =>
 		expect(
-			<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-				<Sidebar open modules={modules} />
-			</TestWrapper>,
+			<Provider store={store}>
+				<ThemeProvider theme={{}}>
+					<MemoryRouter initialEntries={["/Global/second"]}>
+						<Sidebar open modules={modules} />
+					</MemoryRouter>
+				</ThemeProvider>
+			</Provider>,
 			"when mounted",
 			"to satisfy",
-			<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-				<div>
-					<MenuToggle open />
-					<EnhancedMenuItem open icon="cars" title="First page" id="first" label="First page" />
-					<EnhancedMenuItem open icon="person" title="Second page" id="second" label="Second page" />
-					<Logo />
-				</div>
-			</TestWrapper>,
+			<Provider store={store}>
+				<ThemeProvider theme={{}}>
+					<MemoryRouter initialEntries={["/Global/second"]}>
+						<Bar>
+							<MenuToggle open />
+							<EnhancedMenuItem open icon="cars" title="First page" id="first" label="First page" />
+							<EnhancedMenuItem open icon="person" title="Second page" id="second" label="Second page" />
+							<Logo />
+						</Bar>
+					</MemoryRouter>
+				</ThemeProvider>
+			</Provider>,
 		));
 
 	it("renders a minimal sidebar", () =>
 		expect(
-			<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-				<Sidebar />
-			</TestWrapper>,
+			<Provider store={store}>
+				<ThemeProvider theme={{}}>
+					<MemoryRouter>
+						<Sidebar />
+					</MemoryRouter>
+				</ThemeProvider>
+			</Provider>,
 			"when mounted",
 			"to satisfy",
-			<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-				<div>
-					<MenuToggle />
-					<Logo />
-				</div>
-			</TestWrapper>,
+			<Provider store={store}>
+				<ThemeProvider theme={{}}>
+					<MemoryRouter>
+						<Bar>
+							<MenuToggle />
+							<Logo />
+						</Bar>
+					</MemoryRouter>
+				</ThemeProvider>
+			</Provider>,
 		));
 });
 
@@ -158,26 +178,34 @@ describe("EnhancedMenuItem", () => {
 
 	it("renders a MenuItem with href", () =>
 		expect(
-			<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-				<EnhancedMenuItem id="route" path="/Global/somewhere" />
-			</TestWrapper>,
+			<Provider store={store}>
+				<MemoryRouter>
+					<EnhancedMenuItem id="route" path="/Global/somewhere" />
+				</MemoryRouter>
+			</Provider>,
 			"when mounted",
 			"to satisfy",
-			<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-				<MenuItem href="/Global/route" />
-			</TestWrapper>,
+			<Provider store={store}>
+				<MemoryRouter>
+					<MenuItem href="/Global/route" />
+				</MemoryRouter>
+			</Provider>,
 		));
 
 	it("sets the active flag if path matches href", () =>
 		expect(
-			<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-				<EnhancedMenuItem id="route" />
-			</TestWrapper>,
+			<Provider store={store}>
+				<MemoryRouter initialEntries={["/Global/route/subpath"]}>
+					<EnhancedMenuItem id="route" />
+				</MemoryRouter>
+			</Provider>,
 			"when mounted",
 			"to satisfy",
-			<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-				<MenuItem active href="/Global/route" />
-			</TestWrapper>,
+			<Provider store={store}>
+				<MemoryRouter>
+					<MenuItem active href="/Global/route" />
+				</MemoryRouter>
+			</Provider>,
 		));
 });
 
@@ -195,26 +223,78 @@ describe("MenuToggle", () => {
 	describe("with no theme", () => {
 		it("renders a MenuItem with specific settings", () =>
 			expect(
-				<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-					<MenuToggle toggle={toggle} />
-				</TestWrapper>,
+				<Provider store={store}>
+					<ThemeProvider theme={{ foo: "bar" }}>
+						<MemoryRouter>
+							<MenuToggle toggle={toggle} />
+						</MemoryRouter>
+					</ThemeProvider>
+				</Provider>,
 				"when mounted",
 				"to satisfy",
-				<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-					<MenuItem id="sidebarMenuToggle" menuToggle icon="expand" onClick={expect.it("to be a function")} />
-				</TestWrapper>,
+				<Provider store={store}>
+					<MenuItem id="sidebarMenuToggle" menuToggle icon="menu" onClick={expect.it("to be a function")} />
+				</Provider>,
 			));
 
 		it("renders as open", () =>
 			expect(
-				<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-					<MenuToggle toggle={toggle} open />
-				</TestWrapper>,
+				<Provider store={store}>
+					<ThemeProvider theme={{ foo: "bar" }}>
+						<MemoryRouter>
+							<MenuToggle toggle={toggle} open />
+						</MemoryRouter>
+					</ThemeProvider>
+				</Provider>,
 				"when mounted",
 				"to satisfy",
-				<TestWrapper provider={{ store }} stylesProvider muiThemeProvider memoryRouter>
-					<MenuItem id="sidebarMenuToggle" menuToggle open icon="collapse" onClick={expect.it("to be a function")} />
-				</TestWrapper>,
+				<Provider store={store}>
+					<MenuItem id="sidebarMenuToggle" menuToggle open icon="layers" onClick={expect.it("to be a function")} />
+				</Provider>,
+			));
+	});
+
+	describe("with theme values", () => {
+		let theme;
+		beforeEach(() => {
+			theme = {
+				icons: {
+					sidebarOpen: "open",
+					sidebarClosed: "closed",
+				},
+			};
+		});
+
+		it("renders a MenuItem with specific settings", () =>
+			expect(
+				<Provider store={store}>
+					<ThemeProvider theme={theme}>
+						<MemoryRouter>
+							<MenuToggle toggle={toggle} />
+						</MemoryRouter>
+					</ThemeProvider>
+				</Provider>,
+				"when mounted",
+				"to satisfy",
+				<Provider store={store}>
+					<MenuItem menuToggle icon="closed" onClick={expect.it("to be a function")} />
+				</Provider>,
+			));
+
+		it("renders as open", () =>
+			expect(
+				<Provider store={store}>
+					<ThemeProvider theme={theme}>
+						<MemoryRouter>
+							<MenuToggle toggle={toggle} open />
+						</MemoryRouter>
+					</ThemeProvider>
+				</Provider>,
+				"when mounted",
+				"to satisfy",
+				<Provider store={store}>
+					<MenuItem menuToggle open icon="open" onClick={expect.it("to be a function")} />
+				</Provider>,
 			));
 	});
 });
